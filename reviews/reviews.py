fed45767c0ac61e747cae27bc1e5f138ebb70894
Ted Skyvell <tskyvell@gmail.com>
2021-04-01 18:45:34 +0000

    Added compute hash function and layed out what review and delegation data to store.

--- reviews/reviews.py
+++ reviews/reviews.py
@@ -1,4 +1,4 @@
-from iconservice import IconScoreBase, VarDB, Logger, IconScoreDatabase, external, Address, revert, payable
+from iconservice import IconScoreBase, VarDB, Logger, IconScoreDatabase, external, Address, revert, payable, DictDB, sha_256
 from .constants import SYSTEM_SCORE_ADDRESS, TRANSCRANIAL_SOLUTIONS_ADDRESS
 from .interface import SystemScoreInterface
 from .checks import only_owner
@@ -10,9 +10,28 @@ class Reviews(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
+        # Owneradjustable settings.
         self._bad_review_threshold = VarDB('_bad_review_threshold', db, int)
         self._delegation_threshold = VarDB('_delegation_threshhold', db, int)
         self._duration_per_icx = VarDB('_duration_per_icx', db, int)
+
+        # Need these to loop.
+        self._active_review_count = VarDB(
+            '_active_review_count', db, value_type=int)
+        self._index_to_guid = DictDB('_index_to_guid', db, value_type=int)
+        self._guid_to_index = DictDB('_guid_to_index', db, value_type=int)
+
+        # Store reviewdata and keep track of delegations and staking.
+        self._review_hash = DictDB('_review_hash', db, value_type=str)
+        self._start_block = DictDB('_start_block', db, value_type=int)
+        self._expiration_time = DictDB('_expiration_time', db, value_type=int)
+        self._reviewer_address = DictDB(
+            '_reviewer_address', db, value_type=Address)
+        self._delegated_icx = DictDB(
+            '_delegated_icx', db, value_type=int)  # In loop?
+        self._delegated_to = DictDB('_delegated_to', db, value_type=Address)
+
+        # System score interface.
         self._system_interface = IconScoreBase.create_interface_score(
             SYSTEM_SCORE_ADDRESS, SystemScoreInterface)
 
@@ -68,7 +87,14 @@ class Reviews(IconScoreBase):
         self._system_interface.setStake(self.msg.value)
         self._system_interface.setDelegation(delegation)
 
-        # TODO Store delegation and review data.
+        # Store review and delegation data associated with this guid.
+        self._review_hash[guid] = self._compute_review_hash(
+            guid, review_message, review_score, self._compute_review_expiration(), prep, self.msg.sender)
+        self._delegated_icx[guid] = self.msg.value
+        self._delegated_to[guid] = delegation[0]['address']
+        self._reviewer_address = self.msg.sender
+        self._expiration_time = self._compute_review_expiration()
+
 
 # ==================== Util ====================
 
@@ -91,3 +117,11 @@ class Reviews(IconScoreBase):
         Returns the the expiration time in microseconds.
         """
         return self.now() + self.msg.value * self._duration_per_icx.get()
+
+    def _compute_review_hash(self, guid: int, review_message: Address, review_score: int, expiration: int, prep: Address, reviewer: Address) -> int:
+        """
+        Compute sha256 hash of review.
+        """
+        byte_data = bytes(str(guid) + review_message +
+                          str(review_score) + str(expiration) + prep + reviewer, 'utf-8')
+        return sha_256(byte_data).hex()
