179f96ef6e3a16729b77dca5c7f7a3aac69514af
Ted Skyvell <tskyvell@gmail.com>
2021-04-04 23:11:51 +0000

    Moved constants file.

--- reviews/constants.py
+++
@@ -1,7 +0,0 @@
-from iconservice import Address
-
-SYSTEM_SCORE_ADDRESS = Address.from_string(
-    'cx0000000000000000000000000000000000000000')
-
-TRANSCRANIAL_SOLUTIONS_ADDRESS = Address.from_string(
-    'hx2f3fb9a9ff98df2145936d2bfcaa3837a289496b')
