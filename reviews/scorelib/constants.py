2ba9e9440dee704d6390e1097355d5eddef0d0dd
Ted Skyvell <tskyvell@gmail.com>
2021-04-04 23:09:23 +0000

    Some storage types needed for our purposes.

---
+++ reviews/scorelib/constants.py
@@ -0,0 +1,9 @@
+from iconservice import Address
+
+SYSTEM_SCORE_ADDRESS = Address.from_string(
+    'cx0000000000000000000000000000000000000000')
+
+TRANSCRANIAL_SOLUTIONS_ADDRESS = Address.from_string(
+    'hx2f3fb9a9ff98df2145936d2bfcaa3837a289496b')
+
+MAX_ITERATION_LOOP = 100
