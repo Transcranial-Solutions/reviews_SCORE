1c0c6fcce5ecb8360a5912b9543ee253b7bbf61d
Ted Skyvell <tskyvell@gmail.com>
2021-04-15 18:23:19 +0000

    Added staking contract.

---
+++ staking/tests/test_unit_staking.py
@@ -0,0 +1,12 @@
+from ..staking import Staking
+from tbears.libs.scoretest.score_test_case import ScoreTestCase
+
+
+class TestStaking(ScoreTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.score = self.get_score_instance(Staking, self.test_account1)
+
+    def test_hello(self):
+        self.assertEqual(self.score.hello(), "Hello")
