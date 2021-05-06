138066104b5e0770186b1703deece393147f2eed
steven.johnson <steven.johnson@soulmachines.com>
2021-05-06 21:57:00 +0000

    testing skeletons

--- reviews/tests/test_unit_review.py
+++ reviews/tests/test_unit_review.py
@@ -1,12 +1,11 @@
+from ..review_handler import _Review, ReviewHandler
 from ..reviews import Reviews
 from tbears.libs.scoretest.score_test_case import ScoreTestCase
 
-
-class TestReviews(ScoreTestCase):
-
+class TestReview(ScoreTestCase):
     def setUp(self):
         super().setUp()
         self.score = self.get_score_instance(Reviews, self.test_account1)
 
-    # def test_hello(self):
-    #    self.assertEqual(self.score.hello(), "Hello")
+    def test_yaaa(self):
+        pass
---
+++ reviews/tests/test_unit_review_handler.py
@@ -0,0 +1,26 @@
+from ..reviews import Reviews
+from ..review_handler import ReviewHandler
+from tbears.libs.scoretest.score_test_case import ScoreTestCase
+
+
+class TestReviewHandler(ScoreTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.score = self.get_score_instance(Reviews, self.test_account1)
+
+    def test_create_review(self):
+        pass
+
+    def test_remove_review(self):
+        pass
+    
+    def test_get_review(self):
+        pass
+
+    def test_get_reviews(self):
+        pass
+
+    def test_get_all_reviews(self):
+        pass
+
