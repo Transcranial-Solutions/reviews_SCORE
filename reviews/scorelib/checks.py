f2a1737ffc30acdf255977423203c314c7a685ce
Ted Skyvell <tskyvell@gmail.com>
2021-05-06 01:12:28 +0000

    Removed tag.

--- reviews/scorelib/checks.py
+++ reviews/scorelib/checks.py
@@ -1,80 +1,13 @@
-from iconservice import isfunction, wraps, revert, Logger
-
-# ================================================
-#  Exceptions
-# ================================================
-
-
-class SenderNotScoreOwnerError(Exception):
-    pass
-
-
-class SenderNotAuthorized(Exception):
-    pass
-
-
-class SenderNotGovernance(Exception):
-    pass
-
-
-class NotAFunctionError(Exception):
-    pass
-
-
-def only_governance(func):
-    if not isfunction(func):
-        raise NotAFunctionError
-
-    @wraps(func)
-    def __wrapper(self: object, *args, **kwargs):
-        if self.msg.sender != self._governance.get():
-            raise SenderNotGovernance(self.msg.sender)
-
-        return func(self, *args, **kwargs)
-    return __wrapper
-
+from iconservice import isfunction, revert, wraps
 
 def only_admin(func):
-    if not isfunction(func):
-        raise NotAFunctionError
+	if not isfunction(func):
+		revert(f"NotAFunctionError")
 
-    @wraps(func)
-    def __wrapper(self: object, *args, **kwargs):
-        if self.msg.sender != self._admin.get():
-            raise SenderNotAuthorized(self.msg.sender)
+	@wraps(func)
+	def __wrapper(self: object, *args, **kwargs):
+		if self.msg.sender != self._admin.get():
+			revert(f"SenderNotAdmin: sender({self.msg.sender}), admin({self._admin.get()})")
 
-        return func(self, *args, **kwargs)
-    return __wrapper
-
-
-def only_owner(func):
-    if not isfunction(func):
-        raise NotAFunctionError
-
-    @wraps(func)
-    def __wrapper(self: object, *args, **kwargs):
-        if self.msg.sender != self.owner:
-            raise SenderNotScoreOwnerError(self.owner)
-
-        return func(self, *args, **kwargs)
-    return __wrapper
-
-
-def catch_error(func):
-    if not isfunction(func):
-        raise NotAFunctionError
-
-    @wraps(func)
-    def __wrapper(self: object, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except BaseException as e:
-            Logger.error(repr(e))
-            try:
-                # readonly methods cannot emit eventlogs
-                self.ShowException(repr(e))
-            except:
-                pass
-            revert(repr(e))
-
-    return __wrapper
+		return func(self, *args, **kwargs)
+	return __wrapper
