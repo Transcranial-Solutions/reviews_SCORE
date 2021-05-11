2caa7e31ec56769d3c37e4c5a1825d087c94c570
Ted Skyvell <tskyvell@gmail.com>
2021-05-11 20:38:30 +0000

    .

--- staking/tests/test_unit_staking.py
+++ staking/tests/test_unit_staking.py
@@ -1,6 +1,7 @@
 from ..staking import Staking
 from ..scorelib.constants import Score
 from tbears.libs.scoretest.score_test_case import ScoreTestCase
+from iconservice import Address
 
 
 class TestStaking(ScoreTestCase):
@@ -8,9 +9,10 @@ class TestStaking(ScoreTestCase):
     def setUp(self):
         super().setUp()
         self.score = self.get_score_instance(Staking, self.test_account1)
+        self.gov_score = Address.from_string("cx0000000000000000000000000000000000000000")
 
     def test_increment_funds(self):
-        self.register_interface_score(Score.system)
+        self.register_interface_score(self.gov_score)
         #self.assert_internal_call(Score.system, "setStake")
         self.assertEqual(self.score._total_delegation.get(), 0)
         self.score._increment_funds(40)
