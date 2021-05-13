9a0443189662381b8bfebc1bda5de2d23a821c0b
Ted Skyvell <tskyvell@gmail.com>
2021-05-13 21:26:49 +0000

    We don't assign ascore addresses ourselves. We get assigned a score address when we deploy to cx0....0.

--- reviews/tests/test_integrate_reviews.py
+++ reviews/tests/test_integrate_reviews.py
@@ -31,13 +31,15 @@ class TestTest(IconIntegrateTestBase):
 
         # install SCORE
 
-        self._score_address = self._deploy_score()["scoreAddress"]
+        self._review_score_address = self._deploy_score()["scoreAddress"]
         self._staking_score_address = self._deploy_staking_score()["scoreAddress"]
         self._test_guid = uuid.uuid4().int
         self._message = r"People always ask me about transcranial solutions, It's fantastic. Let me tell you about transcranial solutions. I do very well with transcranial solutions. I love transcranial solutions. No one loves transcranial solutions more than me, BELIEVE ME. transcranial solutions loves me. We're going to have so many transcranial solutions you are going to get sick of transcranial solutions. The transcranial solutions just got 10 feet higher. I have the best transcranial solutions."
         self._review_score = 5
         self._expiration = 10
         self._prep = "hx2f3fb9a9ff98df2145936d2bfcaa3837a289496b"
+        self._set_staking_score()
+
 
     def _deploy_score(self, to: str = SCORE_INSTALL_ADDRESS) -> dict:
         # Generates an instance of transaction for deploying SCORE.
@@ -64,7 +66,7 @@ class TestTest(IconIntegrateTestBase):
 
         return tx_result
 
-    def _deploy_staking_score(self, to: str = STAKING_SCORE_INSTALL_ADDRESS) -> dict:
+    def _deploy_staking_score(self, to: str = SCORE_INSTALL_ADDRESS) -> dict:
         staking_dir = os.path.abspath(os.path.join(DIR_PATH, "../../staking"))
         transaction = (
             DeployTransactionBuilder()
@@ -87,31 +89,36 @@ class TestTest(IconIntegrateTestBase):
 
         return tx_result
 
+    def _set_staking_score(self):
+        call = (
+            CallTransactionBuilder()
+            .from_(self._test1.get_address())
+            .to(self._review_score_address)
+            .step_limit(100_000_000)
+            .nid(3)
+            .nonce(100)
+            .method("set_staking_score")
+            .params({"score": self._staking_score_address})
+            .build()
+        )
+
+        signed_trans = SignedTransaction(call, self._test1)
+        response = self.process_transaction(signed_trans, self.icon_service)
+
+        return response
+
     def test_score_update(self):
         # update SCORE
-        tx_result = self._deploy_score(self._score_address)
+        tx_result = self._deploy_score(self._review_score_address)
 
-        self.assertEqual(self._score_address, tx_result["scoreAddress"])
+        self.assertEqual(self._review_score_address, tx_result["scoreAddress"])
 
         tx_result = self._deploy_staking_score(self._staking_score_address)
 
         self.assertEqual(self._staking_score_address, tx_result["scoreAddress"])
 
     def test_set_staking_score(self):
-        call = (
-            CallTransactionBuilder()
-            .from_(self._test1.get_address())
-            .to(self._score_address)
-            .step_limit(100_000_000)
-            .nid(3)
-            .nonce(100)
-            .method("set_staking_score")
-            .params({"score": STAKING_SCORE_INSTALL_ADDRESS})
-            .build()
-        )
-
-        signed_trans = SignedTransaction(call, self._test1)
-        response = self.process_transaction(signed_trans, self.icon_service)
+        response = self._set_staking_score()
 
         self.assertEqual(True, response["status"])
 
@@ -130,7 +137,7 @@ class TestTest(IconIntegrateTestBase):
         call = (
             CallTransactionBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .step_limit(100_000_000)
             .value(10)
             .nid(3)
@@ -154,7 +161,7 @@ class TestTest(IconIntegrateTestBase):
         call = (
             CallBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .method("get_review")
             .params({"guid": self._test_guid})
             .build()
@@ -167,7 +174,7 @@ class TestTest(IconIntegrateTestBase):
         call = (
             CallBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .method("authenticate_review")
             .params(
                 {
@@ -187,7 +194,7 @@ class TestTest(IconIntegrateTestBase):
         self.assertEqual("0x1", response)
 
     def test_remove_review(self):
-
+        
         # Add review.
         byte_data = bytes(
             str(self._test_guid)
@@ -199,11 +206,10 @@ class TestTest(IconIntegrateTestBase):
             "utf-8",
         )
         msg_hash = sha256(byte_data).hexdigest()
-
         call = (
             CallTransactionBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .step_limit(100_000_000)
             .value(10)
             .nid(3)
@@ -218,29 +224,26 @@ class TestTest(IconIntegrateTestBase):
             )
             .build()
         )
-
         signed_trans = SignedTransaction(call, self._test1)
         response = self.process_transaction(signed_trans, self.icon_service)
-
+        
         # Check for review.
         call = (
             CallBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .method("get_review")
             .params({"guid": self._test_guid})
             .build()
         )
-
         response = self.process_call(call, self.icon_service)
-
         self.assertEqual(self._test_guid, response["guid"])
-
+        
         # Remove review.
         call = (
             CallTransactionBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .step_limit(100_000_000)
             .nid(3)
             .nonce(100)
@@ -248,22 +251,18 @@ class TestTest(IconIntegrateTestBase):
             .params({"guid": self._test_guid})
             .build()
         )
-
         signed_trans = SignedTransaction(call, self._test1)
         response = self.process_transaction(signed_trans, self.icon_service)
-
         self.assertEqual(True, response["status"])
-
+        
         # Check if review removed.
         call = (
             CallBuilder()
             .from_(self._test1.get_address())
-            .to(self._score_address)
+            .to(self._review_score_address)
             .method("get_review")
             .params({"guid": self._test_guid})
             .build()
         )
-
         response = self.process_call(call, self.icon_service)
-
         self.assertEqual(0, response["guid"])
