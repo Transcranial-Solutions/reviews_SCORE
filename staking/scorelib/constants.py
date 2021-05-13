e21946da3366ab81679b3da07933e14dbc965523
Ted Skyvell <tskyvell@gmail.com>
2021-05-13 21:27:15 +0000

    .

--- staking/scorelib/constants.py
+++ staking/scorelib/constants.py
@@ -6,7 +6,7 @@ class Prep:
     geonode = Address.from_string("hx83c0fc2bcac7ecb3928539e0256e29fc371b5078")
 
 class Score:
-    system = Address.from_string('cx0000000000000000000000000000000000000000')
+    system = Address.from_string("cx0000000000000000000000000000000000000000")
 
 
 MAX_ITERATION_LOOP = 100
